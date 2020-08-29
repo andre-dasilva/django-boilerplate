35a4b19ff093a3393d94080ac7ce81c04f57ef0e
André da Silva <andre-da.silva@outlook.com>
2020-08-29 17:10:06 +0200

    Update logging.py to include a timed rotating file logger

--- boilerplate/config/settings/logging.py
+++ boilerplate/config/settings/logging.py
@@ -1,13 +1,30 @@
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        },
+        'verbose': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s'
+        }
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'simple'
         },
+        'file': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+            'when': 'D',
+            'backupCount': 0,
+            'filename': './logs/boilerplate.log'
+        }
     },
     'root': {
-        'handlers': ['console'],
+        'handlers': ['console', 'file'],
         'level': 'DEBUG',
     },
 }
