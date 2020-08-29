0f5121127a1362bedb040671c57374fd0176c383
André da Silva <andre-da.silva@outlook.com>
2020-08-29 16:57:03 +0200

    Add relationship for task to user

--- boilerplate/tasks/models.py
+++ boilerplate/tasks/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Task(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
