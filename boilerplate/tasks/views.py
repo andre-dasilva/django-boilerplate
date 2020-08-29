4df0524a1e12533432b1ad581117a883453ebb8c
André da Silva <andre-da.silva@outlook.com>
2020-08-29 16:49:23 +0200

    Add example logging
    * Logs everything to the console

--- boilerplate/tasks/views.py
+++ boilerplate/tasks/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def index(request):
+    logger.info('Hello there I am logging')
     return HttpResponse("Hello, world from MVC")
